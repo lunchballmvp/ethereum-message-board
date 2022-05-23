c343f1a94c542c4f6fc668e3407a05d11002f1a5
tfrank11 <77943795+tfrank11@users.noreply.github.com>
2022-05-23 13:58:49 -0400

    Fixed bug where app would sometimes crash if MetaMask wasnt installed

--- react.js/eth-msg-brd-web/src/App.tsx
+++ react.js/eth-msg-brd-web/src/App.tsx
@@ -6,30 +6,42 @@ import Overlay from "./components/Overlay";
 import Sidebar from "./components/Sidebar";
 import { getMessages, getNickname } from "./common/api";
 import Chat from "./components/Chat";
+import { Web3Provider } from "@ethersproject/providers";
 
 //Hack to make typescript happy
 declare var window: any;
 
 const CONTRACT_ADDRESS = "0x9555ed8627088CD3d4Cbc7BbdEF27E6543E71322";
 
-// const { ethereum } = window;
-
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-// const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
-
 // Function to check if MetaMask is installed
 function isMetaMaskInstalled() {
-  return Boolean(window.ethereum && window.ethereum.isMetaMask);
+  if (window.ethereum) {
+    return Boolean(window.ethereum && window.ethereum.isMetaMask);
+  } else {
+    return false;
+  }
 }
 
 // Function to check if MetaMask is connected
 async function isMetaMaskConnected() {
-  const { ethereum } = window;
-  const accounts = await ethereum.request({ method: "eth_accounts" });
-  return accounts && accounts.length > 0;
+  try {
+    const { ethereum } = window;
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    return accounts && accounts.length > 0;
+  } catch (err) {
+    return false;
+  }
 }
 
 function App() {
+  // Hacky way to avoid crash if MetaMask is not installed
+  let provider: Web3Provider | undefined;
+  try {
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+  } catch (err) {
+    provider = undefined;
+  }
+
   const [connected, setConnected] = useState(true);
   const [installed, setInstalled] = useState(true);
   const [userAddress, setUserAddress] = useState("");
@@ -45,18 +57,22 @@ function App() {
   initialise();
 
   //event listener to call initialise when accounts change
-  window.ethereum.on("accountsChanged", async () => {
-    initialise();
-  });
+  window &&
+    window.ethereum &&
+    window.ethereum.on("accountsChanged", async () => {
+      initialise();
+    });
 
   //prompt user to connect their wallet on first render
   useEffect(() => {
     (async () => {
-      await provider.send("eth_requestAccounts", []);
-      const signer = provider.getSigner();
-      const address = await signer.getAddress();
-      setUserAddress(address);
-      setContract(new ethers.Contract(CONTRACT_ADDRESS, ABI, signer));
+      if (provider) {
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setUserAddress(address);
+        setContract(new ethers.Contract(CONTRACT_ADDRESS, ABI, signer));
+      }
     })();
   }, []);
 
